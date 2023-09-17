88873540c59298a4fb9952e89b96373709f114f4
Erinaldo <erinaldo2005@gmail.com>
2023-09-17 01:10:23 -0300

    Formatação do código

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -4,22 +4,26 @@ import java.util.Scanner;
 
 /**
  * <h1>DESAFIO CONTROLE FLUXO</h1>
- * O sistema irá efetuar um laço for ao obter a subtração de dois números informados pelo usuário via terminal.
+ * O sistema irá efetuar um laço for ao obter a subtração de dois números
+ * informados pelo usuário via terminal.
  * <p>
- * <b>Note:</b> Leia atentamente a documentação desta classe para desfrutar dos recursos oferecidos pelo autor
+ * <b>Note:</b> Leia atentamente a documentação desta classe para desfrutar dos
+ * recursos oferecidos pelo autor
  * 
  * @author Erinaldo Silva
  * @version 1.0
- * @since   17/09/2023
+ * @since 17/09/2023
  */
 
 public class Contador {
     /**
      * Este método é utilizado para executar um laço for após obter dois parâmetros
-     * @param parametroUm - o primeiro parâmetro do método
+     * 
+     * @param parametroUm   - o primeiro parâmetro do método
      * @param parametroDois - o segundo parâmetro do método
      * @
-     * @throws ParametrosInvalidosException - lançada se o segundo parâmetro for maior que o primeiro
+     * @throws   ParametrosInvalidosException - lançada se o segundo parâmetro for
+     *           maior que o primeiro
      */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
@@ -44,18 +48,18 @@ public class Contador {
         } else {
             int contagem = parametroDois - parametroUm;
             System.out.println("\nQuantidade de iterações For: " + contagem);
-    
+
             for (int i = 1; i <= contagem; i++) {
                 System.out.println("Imprimindo o número " + i);
             }
         }
     }
 
-    static int lerNumero(Scanner numero){
-        while (true){
+    static int lerNumero(Scanner numero) {
+        while (true) {
             try {
                 return numero.nextInt();
-            } catch (InputMismatchException e){
+            } catch (InputMismatchException e) {
                 System.err.println("Para continuar, digite um número inteiro válido!!");
                 numero.next();
             }
