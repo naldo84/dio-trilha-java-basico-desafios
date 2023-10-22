0ef9c0237892f4ee5e7f0250f42420ad4c8bb0d1
Erinaldo <erinaldo2005@gmail.com>
2023-10-22 20:28:04 -0300

    "Exclusão do projeto - Pokemon"

--- lab-padroes-projeto-spring/src/main/java/one/digitalinnovaton/gof/model/AtaquesRepository.java
+++
@@ -1,9 +0,0 @@
-package one.digitalinnovaton.gof.model;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AtaquesRepository extends CrudRepository <Ataques, Long>{
-    
-}
--- lab-padroes-projeto-spring/src/main/java/one/digitalinnovaton/gof/model/PokemonRepository.java
+++
@@ -1,9 +0,0 @@
-package one.digitalinnovaton.gof.model;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PokemonRepository extends CrudRepository<Pokemon, Long>{
-    
-}
