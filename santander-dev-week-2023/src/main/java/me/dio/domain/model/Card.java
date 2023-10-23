fcf656e0e22849d01552307832f4da335adebd60
Erinaldo <erinaldo2005@gmail.com>
2023-10-23 01:17:00 -0300

    Término do model e inclusão do Repository

--- santander-dev-week-2023/src/main/java/me/dio/domain/model/Card.java
+++ santander-dev-week-2023/src/main/java/me/dio/domain/model/Card.java
@@ -1,10 +1,27 @@
 package me.dio.domain.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.math.BigDecimal;
 
+
+@Entity(name = "tb_card")
 public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
+    @Column(unique = true)
     private String numer;
+
+
+    @Column(name = "available_limit", precision = 13, scale = 2)
     private BigDecimal limit;
     
     public Long getId() {
